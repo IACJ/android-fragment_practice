71bcfd7820528c2bce088f25b7a2b728742f9c1a
IACJ <iacjacj@qq.com>
2017-04-07 20:54:53 +0800

    first commit

---
+++ app/src/main/java/com/example/fragmentpractice/News.java
@@ -0,0 +1,26 @@
+package com.example.fragmentpractice;
+
+/**
+ * Created by IACJ on 2017/4/7.
+ */
+
+public class News {
+    private String title;
+    private String content;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
